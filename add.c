8782a0b76cd80fb655f70582c495cd70c17c451e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-11-30 14:10:31 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+ 
+    int num1,num2;
+   printf("Ener a number num1\n");
+   scanf("%d %d",&num1,&num2);
+
+   printf("Enter number num2\n");
+   
+
+   printf("The sum is %d\n",num1+num2);
+
+   return 0;
+
+   }   
