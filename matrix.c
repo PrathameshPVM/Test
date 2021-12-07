1b7e2c62ad85209921bb0fa113a964ca58531a9e
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-08 00:30:03 +0530

    Add files via upload

---
+++ matrix.c
@@ -0,0 +1,17 @@
+//print star
+#include<stdio.h>
+void main()
+{
+    system("cls");
+    int r,c;
+    for ( r = 1; r <= 4; r++)
+    {
+        for ( c = 1; c <= 5; c++)
+    {   
+    printf(" *");
+    }
+        
+       printf("\n"); 
+    }
+    
+}
