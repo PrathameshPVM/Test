13cd7dfb99088986a112b8d83fa47e3af3f8296a
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-09 15:14:25 +0530

    Add files via upload

---
+++ pattern285.c
@@ -0,0 +1,19 @@
+/*EDCBA
+  EDCBA
+  EDCBA
+  EDCBA*/
+  #include <stdio.h>
+  int main()
+  {
+      char r,c,a='A';
+      for ( r = 'D'; r >= 'A'; r--)
+      {
+        for ( c = 'E'; c >= 'A'; c--)
+        {
+            printf("%c",c);
+        }
+        printf("\n");
+      }
+      
+      return 0;
+  }
