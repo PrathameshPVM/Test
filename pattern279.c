4069c369814339bac9416fdfbc0849ac52824208
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-09 14:45:16 +0530

    Add files via upload

---
+++ pattern279.c
@@ -0,0 +1,18 @@
+/*AAAAA
+  BBBBB
+  CCCCC
+  DDDDD*/
+  #include<stdio.h>
+  void main ()
+  {
+      char r,c,a='A';
+      for ( r ='A'; r <= 'D'; r++)     
+       {
+         for ( c = 'A'; c <= 'E'; c++)
+         {
+            printf("%c",r);
+         }
+         printf("\n");
+      }
+      
+  }
