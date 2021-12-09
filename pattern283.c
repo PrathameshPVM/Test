a36884082a16c64fb766fe5c106a2bb54d375eaa
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-09 15:05:22 +0530

    Add files via upload

---
+++ pattern283.c
@@ -0,0 +1,19 @@
+/*54321
+  54321
+  54321
+  54321*/
+  #include <stdio.h>
+  int main()
+  {
+      int r,c;
+      for ( r = 4; r >=1 ; r--)
+      {
+         for ( c = 5; c >= 1; c--)
+         {
+           printf("%d",c);
+         }
+         printf("\n");
+      }
+      
+      return 0;
+  }
