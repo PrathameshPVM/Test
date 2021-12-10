6dc9bee8f6ca5ee878ebf4b4164d6f907814ea82
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-10 14:35:49 +0530

    Add files via upload

---
+++ pattern297.c
@@ -0,0 +1,28 @@
+/*   *
+   *   *
+ *   *   *     */
+#include<stdio.h>
+void main()
+{
+    system("cls");
+    int h,sp,str;
+    
+   
+    for ( h = 1; h <= 3 ; h++)
+    {
+       for ( sp = 0; sp <= 40-h; sp++)
+       {
+           printf(" ");
+        
+       }
+       for ( str = 1; str <= h; str++)
+       {
+           printf(" *");
+       
+       }
+       
+       printf("\n");
+    }
+    
+    
+} 
