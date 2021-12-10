54d64b9e21e09534fa47fcfe458146720b7a07b4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-10 12:41:09 +0530

    Add files via upload

---
+++ Array1.c
@@ -0,0 +1,17 @@
+//array roll 3
+#include <stdio.h>
+int main()
+{
+    int a[3];
+    a[0]=2;
+    a[1]=3;
+    a[2]=6;
+
+    printf("Marks of Roll no 1 %d\n",a[0]);
+    printf("Marks of Roll no 2 %d\n",a[1]);
+    printf("Marks of Roll no 3 %d\n",a[2]);
+
+    
+    
+    return 0;
+}
