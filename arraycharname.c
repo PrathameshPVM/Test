049b845f47d7553071cf4d5dae8fed7b26929ef6
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-16 00:29:21 +0530

    Add files via upload

---
+++ arraycharname.c
@@ -0,0 +1,12 @@
+//program to create char arry to store 5  character in an array
+#include <stdio.h>
+int main()
+{
+    char n[5]={'a','m','i','t','a'};
+    for (char i = 0; i < 5; i++)
+    {
+        printf("%c",n[i]);
+    }
+    
+    return 0;
+}
