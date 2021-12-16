7344d037574caf6f5607c65391328fdd7dcf4cd8
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-16 16:07:38 +0530

    Add files via upload

---
+++ hny.c
@@ -0,0 +1,17 @@
+int main()
+{	
+	printf("\n HAPPY NEW YEAR");
+	line();
+	printf("\nHAPPY \nNEW \nYEAR");
+	line();
+
+}
+void line()
+	{	
+        int i;
+		printf("\n");
+        for(i=1;i<=10;i++)
+		printf("-");
+
+    }
+		
