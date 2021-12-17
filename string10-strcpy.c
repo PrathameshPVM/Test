04f46c38f2cdd79d04d4d6be6670069b6588f79a
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-17 20:23:31 +0530

    Add files via upload

---
+++ string10-strcpy.c
@@ -0,0 +1,13 @@
+//strcpy
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char n1[20]="mkpits" ;
+	char n2[20];
+strcpy(n2,n1);
+printf("\n n1 =%s",n1);
+printf("\n n2 =%s",n2);
+
+    return 0;
+}
