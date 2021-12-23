f508f2b5159ce556f9897c297e333fa4cd8670e4
PrathameshPVM <95204156+PrathameshPVM@users.noreply.github.com>
2021-12-23 16:03:26 +0530

    Add files via upload

---
+++ enum5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+     enum months{jan=1, feb, march, april, may, june, july, august, september, october, november, december};  
+    int main()  
+    {  
+        for (int i = jan; i <= december; i++)
+        {
+            printf("\n %d",i);
+        }
+    }   
